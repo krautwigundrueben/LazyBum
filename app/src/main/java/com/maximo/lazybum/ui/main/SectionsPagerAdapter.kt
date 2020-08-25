332d5b20664354d97a1068ed21f9891ba6530cb3
UltraMax <max.krautwig@gmail.com>
2020-08-25 23:38:49 +0200

    Initial commit

---
+++ app/src/main/java/com/maximo/lazybum/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,40 @@
+package com.maximo.lazybum.ui.main
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.maximo.lazybum.MediaFragment
+import com.maximo.lazybum.SmartHomeFragment
+import com.maximo.lazybum.R
+
+private val TAB_TITLES = arrayOf(
+        R.string.tab_smart_home,
+        R.string.tab_media
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
+    : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        // getItem is called to instantiate the fragment for the given page.
+        when (position) {
+            0 -> return SmartHomeFragment()
+            1 -> return MediaFragment()
+            else -> return SmartHomeFragment()
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        // Show 2 total pages.
+        return 2
+    }
+}
